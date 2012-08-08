39a217baa74efba3c090c0dab94b1f792ff040e2
Joshua Walton <adenine@gmail.com>
2012-08-07 21:13:56 -0400

    Made it so repeat entries to the pool are ignored

--- node_server.js
+++ node_server.js
@@ -21,7 +21,20 @@ wsServer = new WebSocketServer({
 // WebSocket server
 wsServer.on('request', function(request) {
     var connection = request.accept(null, request.origin);
-    var index = clientconnections.push(connection) - 1;
+
+
+    var repeatConnection = false;
+    for (var i; i<clientconnections.length; i++) {
+        if (clientconnections[i] - 1 === index) {
+             repeatConnection = true; // flag if it exists
+             //console.log("rejected existing user, already connected");
+
+        }
+    }
+    if (repeatConnection === false) {
+        var index = clientconnections.push(connection) - 1;
+        //console.log("Logged new connection");
+    }
 
     //console.log(connection);
     // This is the most important callback for us, we'll handle
@@ -29,7 +42,7 @@ wsServer.on('request', function(request) {
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
             // process WebSocket message
-            console.log(message);
+            //console.log(message);
             //console.log(message.utf8Data.name);
             //console.log(JSON.parse(message.utf8Data));
             var tMsg = JSON.parse(message.utf8Data);
@@ -38,16 +51,34 @@ wsServer.on('request', function(request) {
             // console.log(tMsg);
 
             if (tMsg['name']) {
-                console.log("I got sent a name");
-                console.log(tMsg);
-                console.log ("your name will be "+tMsg['name'][0].name);
-                var tVar = [tMsg['name'][0].name, index];
-                clients.push(tVar);
-                console.log(clients);
+                // console.log("I got sent a name");
+                // console.log(tMsg);
+                // console.log ("your name will be "+tMsg['name'][0].name);
+                var tVar = [tMsg['name'][0].name, connection['remoteAddress']];
+
+                var existingClient = false;
+                for(var i=0; i<clients.length; i++) {
+                    if (clients[i][0] === tVar[0]) {
+                        existingClient = true;
+                        console.log("client is already connected");
+                    }
+                }
+                if (existingClient === false) {
+                    clients.push(tVar);
+                    console.log("client added");
+                    //console.log(tVar+" : "+index);
+                }
+
+                console.log("Here are the current clients "+clients.length);
+
+                for (var i=0; i<clients.length; i++) {
+                    console.log(clients[i][0]);
+                }
             }
 
             if (tMsg['admin']) {
-                //var json = JSON.stringify({ type:'message', data: message });
+                console.log(clients);
+                //var json = JSON.stringify({ name:clients, data: clients });
                 clientconnections[index].sendUTF(JSON.stringify(clients));
             }
 
